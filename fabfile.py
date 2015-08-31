fd16882ca48530cae6d48bbf2948412dbbc6fcf7
Christoffer Vig <christoffer.vig@comperiosearch.com>
2015-08-31 21:39:50 +0200

    removed stuff and added stuff to readme

--- fabfile.py
+++ fabfile.py
@@ -9,12 +9,6 @@ from fabric.operations import sudo, run, put, local
 from fabric.state import env
 from fabric.utils import error
 
-ANACONDA_MD5 = 'c3100392685b5a62c8509c0588ce9376'
-ANACONDA_URL = 'https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh'
-ANACONDA_FN = 'Anaconda-2.3.0-Linux-x86_64.sh'
-ANACONDA_INSTALL_PATH = '/opt/anaconda'
-
-ESLIB_INSTALL_PATH = '/opt/eslib'
 
 
 def provision_server():
@@ -26,42 +20,6 @@ def provision_server():
 
 def restart_server():
     sudo('shutdown -r now')
-
-
-def anaconda_downloaded():
-    r = run('test -f %s' % ANACONDA_FN, quiet=True)
-
-    if getattr(r, 'return_code') != 0:
-        return False
-
-    r = run('md5sum %s' % ANACONDA_FN)
-    md5, _ = getattr(r, 'stdout').split()
-
-    if md5 != ANACONDA_MD5:
-        if confirm("Anaconda archive corrupt. Delete?"):
-            run('rm %s' % ANACONDA_FN)
-        else:
-            disconnect_all()
-            sys.exit(1)
-
-    return True
-
-
-def anaconda_installed():
-    r = run('test -d %s' % ANACONDA_INSTALL_PATH, quiet=True)
-
-    if getattr(r, 'return_code') != 0:
-        return False
-
-    return True
-
-
-def install_anaconda():
-    if not anaconda_downloaded():
-        run('wget --quiet %s' % ANACONDA_URL)
-    if not anaconda_installed():
-        sudo('bash %s -b -p %s' % (ANACONDA_FN, ANACONDA_INSTALL_PATH))
-        sudo('echo "export PATH=/opt/anaconda/bin:$PATH" > /etc/profile.d/anaconda.sh')
 
 
 def package_installed(pkg):
@@ -101,32 +59,6 @@ def install_logstash():
         sudo('apt-get install logstash -yq')
 
 
-def install_neo4j():
-    if not file_exists(NEO4J_FN):
-        run('wget -q %s -O %s' % (NEO4J_URL, NEO4J_FN))
-
-    sudo('adduser --home /home/neo4j --system --shell /bin/bash neo4j')
-    sudo('(cd /home/neo4j; tar zxf ~/%s)' % NEO4J_FN, user='neo4j')
-    sudo('yes neo4j|(HEADLESS=true; /home/neo4j/%s/bin/neo4j-installer install)' % NEO4J_FOLDER)
-    sudo('echo "neo4j   soft    nofile  40000" > /etc/security/limits.conf')
-    sudo('echo "neo4j   hard    nofile  40000" >> /etc/security/limits.conf')
-    sudo('echo "session required pam_limits.so" > /etc/pam.d/common-session')
-    sudo('echo "session required pam_limits.so" > /etc/pam.d/common-session-noninteractive')
-    put('provision/neo4j-server.properties', '.')
-    sudo('mv neo4j-server.properties /home/neo4j/%s/conf/neo4j-server.properties' % NEO4J_FOLDER)
-    sudo('chown neo4j /home/neo4j/%s/conf/neo4j-server.properties' % NEO4J_FOLDER)
-    sudo('chmod 644 /home/neo4j/%s/conf/neo4j-server.properties' % NEO4J_FOLDER)
-    sudo('service neo4j-service start')
-
-
-def install_neo4j_user():
-    put('provision/auth', '.')
-    sudo('mv auth /home/neo4j/neo4j-community-2.2.2/data/dbms/auth')
-    sudo('chown neo4j /home/neo4j/neo4j-community-2.2.2/data/dbms/auth')
-    sudo('chmod 600 /home/neo4j/neo4j-community-2.2.2/data/dbms/auth')
-    sudo('service neo4j-service restart')
-
-
 def file_exists(fn):
     r = run('test -f %s' %fn, quiet=True)
 
@@ -144,22 +76,6 @@ def install_kibana():
             run('npm install -g npm@3.2')
             run('source /home/vagrant/.bashrc && npm install')
 
-
-def install_eslib():
-    if file_exists(ESLIB_INSTALL_PATH):
-        return
-
-    install_debian_packages(['git', 'python-dev'])
-
-    sudo('(git clone https://github.com/comperiosearch/elasticsearch-eslib.git %s)' % ESLIB_INSTALL_PATH)
-    sudo('pip install -e /opt/eslib')
-    sudo('(chmod -R a+w %s)' % ESLIB_INSTALL_PATH)
-
-
-def install_self():
-    local('git archive master -o master.zip --format zip --prefix twitter-analysis-lab/')
-    put('master.zip', '.')
-    sudo('unzip master')
 
 def vagrant():
     env.user = 'vagrant'
@@ -181,15 +97,6 @@ def install_debian_packages(packages=None):
     if packages:
         sudo('apt-get install -qq -y %s' % ' '.join(packages))
 
-
-def place_credentials():
-    boto_fn = 'config/boto'
-
-    if not os.path.exists(boto_fn):
-        error('Could not find boto configuration file ...')
-
-    put(boto_fn, '.')
-    sudo('mv boto .boto')
 
 def provision_data():
     with cd('twitter-analysis-lab'):
--- readme.md
+++ readme.md
@@ -1,5 +1,4 @@
-Repository for Javazone-box with elasticsearch 2.0-beta and Kibana 4.2 
-
+#Fabric provisioned vagrant box with Ubuntu 15.4, Elasticsearch 2.0 beta 1 debian and Kibana from source.
 
 Requires fabric
 ```
@@ -9,3 +8,12 @@ pip install fabric
 To provision Vagrant dev image:
 
 `fab vagrant provision_server`
+
+You need to do a few manual steps to kick off everything.
+Log on to the box and install nvm and stuf
+
+    vagrant ssh
+    sudo service elasticsearch start
+    cd kibana
+    nvm install $(cat .node-version)
+    bin/kibana --dev
